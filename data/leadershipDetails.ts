6ca58c5c449ea3319d4caceb940035bc3d4ad73d
David Nienge <davidnienge@gmail.com>
2025-08-13 15:06:10 +0100

    leadership page edited

--- data/leadershipDetails.ts
+++ data/leadershipDetails.ts
@@ -6,16 +6,17 @@ export const leadershipDetails = {
     {
       title: "Arome Tokula",
       description: `
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
+        Rev. Arome E. Tokula is the Senior Pastor of Christ Family Center of CFC Abuja. With his team of pastors and ministers he gives leadership and pastoral oversight to the members of CFC Abuja. With a strong teaching anointing and unprecedented skill, he brings God's Word to you in very simple, practical, relevant and applicable ways.
+        Pastor Arome travels across the length and breadth of Nigeria and the world ministering with a blend of the prophetic and tangible anointing to bring healing and increase to his hearers.
+        With a heart burning to see the kingdom of God established in the lives of people, he preaches to his hearers, inspiring faith, breaking them into supernatural kingdom wealth, and bringing healing.
       `,
       image: "/assets/hug.png",
     },
     {
       //   title: "Arome Tokula",
       description: `
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
+        Pastor Arome Tokula is the Global Lead Pastor of Christ Family Center, a seasoned author of four books and a life coach. With an unprecedented pastoral skill and a dynamic teaching and healing anointing, he ministers God’s Word in simple, yet relevant and applicable ways.
+        He is happily married to Pastor Nguavese Tokula, the Global Director of Churches of Christ Family Ministry, and they are blessed with three wonderful children: Psalm, Yana, and Justin. 
       `,
       socialIcons: [
         {
@@ -35,16 +36,16 @@ export const leadershipDetails = {
     {
       title: "Avese Tokula",
       description: `
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
+        Nguavese Jennifer Tokula is the Director of Churches at Christ Family Ministry, based in Abuja, Nigeria. With over 20 years of dedicated service in ministry alongside her husband, she has been instrumental in training and developing leaders, as well as raising pastors.
+        She is deeply committed to the calling to share the beautiful message of the Lord Jesus Christ and to nurture believers into maturity in Him. Her leadership is focused not only on growth but also on fostering meaningful impact and lasting influence. 
       `,
       image: "/assets/hug.png",
     },
     {
       //   title: "Avese Tokula",
       description: `
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
+        Pastor Nguavese Tokula is deeply committed to raising fearless, godly leaders of tomorrow who are driven by a love for their society and a desire to make a positive impact. Her vision is centered on empowering individuals to become agents of transformation in their communities.
+        She is happily married to her husband of 16 years, Rev. Arome Tokula, and together they are blessed with three wonderful children. She teaches in the Believers–Beginner’s Class, Minister–Service Class, and Leader–Managerial Class at JJRSF Christian Leadership Academy (CLA).
       `,
       socialIcons: [
         {
@@ -70,23 +71,23 @@ export const leadershipDetails = {
   residentPastors: [
     {
       name: "Simon and Iveren Akaan",
-      desc: "Resident Pastor, CFC Gboko",
-      image: "/assets/pastors.jpeg",
+      desc: "Resident Pastors, CFC Gboko",
+      image: "/assets/pstSimon.jpg",
     },
     {
       name: "Atom and Fanen Ahura",
-      desc: "Resident Pastor, CFC Makurdi",
-      image: "/assets/pastors.jpeg",
+      desc: "Resident Pastors, CFC Makurdi",
+      image: "/assets/revAtom.jpg",
     },
     {
       name: "Abel and Esther Breath",
-      desc: "Resident Pastor, CFC Kaduna",
-      image: "/assets/pastors.jpeg",
+      desc: "Resident Pastors, CFC Kaduna",
+      image: "/assets/pstAbel.jpg",
     },
     {
       name: "John Eze",
-      desc: "Resident Pastor, CFC Sagamu",
-      image: "/assets/pastors.jpeg",
+      desc: "Resident Pastors, CFC Sagamu",
+      image: "/assets/pstJohn.jpg",
     },
   ],
 };
