9410b5c0009db5e3a2a0d84bfc077f81e199ba6f
David Nienge <davidnienge@gmail.com>
2025-09-11 09:17:16 +0100

    new convert

---
+++ app/new-convert/page.tsx
@@ -0,0 +1,9 @@
+import NewConvertPage from "@/containers/newConvert";
+
+export default function NewConvert() {
+  return (
+    <div className="w-ful">
+      <NewConvertPage />
+    </div>
+  );
+}
---
+++ containers/newConvert.tsx
@@ -0,0 +1,17 @@
+export default function NewConvertPage() {
+  return (
+    <div className="w-full py-20 border shadow-lg flex justify-center h-full">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfzC94FktYCx85wKJ6toGdFD4SMgWyvjt-uh9DFRfcnRJ9kfw/viewform?embedded=true"
+        width="640"
+        height="1801"
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+        className="w-full"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+}
