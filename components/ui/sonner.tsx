7d4e62788db3934f372efa14b4644429ab590302
David Nienge <davidnienge@gmail.com>
2026-01-01 08:50:29 +0100

    Update sonner.tsx

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -12,9 +12,9 @@ const Toaster = ({ ...props }: ToasterProps) => {
       className="toaster group"
       style={
         {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
+          "--normal-bg": "blue",
+          "--normal-text": "white",
+          "--normal-border": "rgba(0,0,0,0.1)",
         } as React.CSSProperties
       }
       {...props}
